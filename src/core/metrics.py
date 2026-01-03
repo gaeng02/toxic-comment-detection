d9536c0454a455d501bb87d08e8fb073a7a4f99b
gaeng02 <sondin144@gmail.com>
2026-01-03 10:25:31 +0900

    feat : confusion matrix

--- src/core/metrics.py
+++ src/core/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import json
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 
 
 def evaluate (y_true : np.ndarray, y_pred : np.ndarray, path : str) -> None :
@@ -11,6 +11,9 @@ def evaluate (y_true : np.ndarray, y_pred : np.ndarray, path : str) -> None :
     recall = recall_score(y_true, y_pred)
     f1 = f1_score(y_true, y_pred)
     report = classification_report(y_true, y_pred)
+    
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred, labels=[0, 1]).ravel()
+    
     os.makedirs(os.path.dirname(path), exist_ok = True)
     
     with open(path, "w") as f :
@@ -18,7 +21,15 @@ def evaluate (y_true : np.ndarray, y_pred : np.ndarray, path : str) -> None :
         f.write(f"Precision :: {precision:.4f}\n")
         f.write(f"Recall :: {recall:.4f}\n")
         f.write(f"F1 Score :: {f1:.4f}\n")
-        f.write(report)
+        f.write(report)        
+        f.write("\n\n")
+
+        f.write("Confusion Matrix\n")
+        f.write("                       Real Label\n")
+        f.write("                 Positive   Negative\n")
+        f.write(f"Pred Positive   TP={tp:<6} FP={fp:<6}\n")
+        f.write(f"Pred Negative   FN={fn:<6} TN={tn:<6}\n\n")
+        
         
 def write_time (time : float, path : str, message : str = "") -> None :
     if not os.path.exists(path) :
